014bfa732104039b5212b2764f81f30e75e8677f
Soulaimane Alahiane <salahian@c3r7p1.1337.ma>
2025-09-05 10:17:16 +0100

    add animation portal for exit

--- bonus/render_bonus.c
+++ bonus/render_bonus.c
@@ -6,7 +6,7 @@
 /*   By: salahian <salahian@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/20 14:39:10 by abdel-ha          #+#    #+#             */
-/*   Updated: 2025/09/04 12:32:56 by salahian         ###   ########.fr       */
+/*   Updated: 2025/09/04 14:56:30 by salahian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,6 +61,22 @@ void	animate_door(t_data *data)
 	}
 }
 
+void	player_won(t_data *data)
+{
+	int		y;
+	int		x;
+
+	y = (int)((data->player.y / TILE_SIZE) - TILE_SIZE / 2);
+	x = (int)((data->player.x / TILE_SIZE) - TILE_SIZE / 2);
+	if (x < 0 || y < 0)
+		return ;
+	if (data->map.map[y][x] == 'P')
+	{
+		printf("YOU WON\n");
+		destroy_window(data);
+	}
+}
+
 void	animate_exit(t_data *data)
 {
 	int				x;
@@ -90,31 +106,32 @@ void	animate_exit(t_data *data)
 int	render(t_data *data)
 {
 	// static long	start_time;
-	long	current_time;
+	//long	current_time;
 
 	// start_time = get_time();
 
-	current_time = get_time();
-	if ((current_time - data->start_time) >= (1000 / 30))
-	{
-		data->start_time = current_time;
+	//current_time = get_time();
+	//if ((current_time - data->start_time) >= (1000 / 30))
+	//{
+		//data->start_time = current_time;
 		// printf("hello\n");
 			clear_image(&data->bg1, BLACK);
 			animate_door(data);
 			animate_exit(data);
+			player_won(data);
 			draw_direction_lines(data);
 			draw_map(data);
 			cast_rays(data);
 			create_minimap(data);
 			mlx_put_image_to_window(data->mlx, data->win_2d, data->bg.img, 0, 0);
 			mlx_put_image_to_window(data->mlx, data->win_3d, data->bg1.img, 0, 0);
-			mlx_mouse_move(data->mlx, data->win_3d, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
-	}
-	else
-	{
-			// printf("%ld\n", (current_time - start_time));
-		return 0;
-	}
+			//mlx_mouse_move(data->mlx, data->win_3d, SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2);
+	//}
+	// else
+	// {
+	// 		// printf("%ld\n", (current_time - start_time));
+	// 	return 0;
+	// }
 	
 	// static int	frame_counter;
 
